146bc336315054e83acc95eab96d43b90a6b9029
Trayan Iliev <office@iproduct.org>
2022-07-08 20:16:53 +0300

    presentation 2 added

--- 02-js-intro/js/strings.js
+++ 02-js-intro/js/strings.js
@@ -11,5 +11,5 @@ let s1 = '2 + 2'              // creates a string primitive
 let s2 = new String('2 + 2')  // creates a String object
 console.log(eval(s1))         // returns the number 4
 console.log(eval(s2))         // returns the string "2 + 2"
-console.log(eval(s2.valueOf()))  // returns the number 4
+console.log(typeof s2.valueOf())  // returns the number 4
 console.log(typeof s_prim.substring(0))
