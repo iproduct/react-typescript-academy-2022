3f33e4430de7bc43336738f735ec9a9d8763db6d
Trayan Iliev <office@iproduct.org>
2022-07-08 21:56:31 +0300

    presentation 2 updated

--- 02-js-intro/js/github-promise.js
+++ 02-js-intro/js/github-promise.js
@@ -5,14 +5,16 @@ function demo() {
         .then(users => {
             console.log(users);
             return fetch(`https://api.github.com/users/${users[0].username}`);
-        }).then(githubResp => githubResp.json())
+        })
+        .then(githubResp => githubResp.json())
         .then(gitUser => {
             console.log(gitUser);
+            document.createElement(div);
             const img = new Image();
             img.src = gitUser.avatar_url
             resultsElem.appendChild(img);
             return new Promise((resolve, reject) => {
-                setTimeout(resolve, 10000, img)
+                setTimeout(resolve, 5000, img)
             });
         }).then(img => {
             resultsElem.removeChild(img);
