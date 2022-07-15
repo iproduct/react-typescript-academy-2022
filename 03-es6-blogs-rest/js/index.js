8934c17e36ef23b8042fb642682c0ddf1bac76b9
Trayan Iliev <office@iproduct.org>
2022-07-15 18:48:11 +0300

    appendPost() added

--- 03-es6-blogs-rest/js/index.js
+++ 03-es6-blogs-rest/js/index.js
@@ -1,18 +1,20 @@
 const API_BASE_URL = "http://localhost:3000/api";
 const results = document.getElementById("results");
+const form = document.getElementById("add-post-form");
 
 async function fetchPosts() {
     const postsResp = await fetch(`${API_BASE_URL}/posts`);
     const posts = await postsResp.json();
     console.log(posts);
-    posts.map(post => {
-        const div =  document.createElement("div");
-        div.className = 'post-item';
-        div.innerHTML=`<h3>${post.id}: ${post.title}</h3><p>Author: ${post.author}</p>`;
-        return div;
-    }).forEach(div => {
-        results.appendChild(div);
-    });
+    results.innerHTML = '';
+    posts.forEach(appendPost);
+}
+
+function appendPost(post) {
+    const div =  document.createElement("div");
+    div.className = 'post-item';
+    div.innerHTML=`<h3>${post.id}: ${post.title}</h3><p>Author: ${post.author}</p>`;
+    results.appendChild(div);
 }
 
 fetchPosts();
