1d56d39ceb82e65111b401e9c66ce062857cb59b
Trayan Iliev <office@iproduct.org>
2022-08-04 21:25:46 +0300

    all post fetched and presented

--- 05-ts-blogs-rest/src/index.ts
+++ 05-ts-blogs-rest/src/index.ts
@@ -1,20 +1,5 @@
-import { NumberIdGenerator } from './dao/id-generator.js';
-import { UserRepository, UserRepositoryImpl } from './dao/user-repository.js';
-import { Admin, Author, Reader, User } from './model/user.js';
+import { BlogsController } from './controller/blogs-controller.js';
 
-const users: User[] = [
-    new Reader('Trayan', 'Iliev', 'trayan@gmail.com', 'trayan123', { country: 'BG', address: 'Sofia, 1000' }),
-    new Author('John', 'Doe', 'john@gmail.com', 'john123', { country: 'USA', address: 'New York' }),
-    new Admin('Jane', 'Doe', 'jane@gmail.com', 'jane123', { country: 'USA', address: 'San Francisko' }),
-];
+const blogsController = new BlogsController();
+blogsController.init();
 
-const userRepo: UserRepository = new UserRepositoryImpl(new NumberIdGenerator());
-users.forEach(user => userRepo.create(user));
-
-const allUsers = userRepo.findAll();
-const trayan = userRepo.findByEmail('trayan@gmail.com');
-console.log('Found by email:', trayan);
-const results = document.getElementById('results');
-if (results) {
-    results.innerHTML = allUsers.map(user => user.toString()).join('<br>');
-}
