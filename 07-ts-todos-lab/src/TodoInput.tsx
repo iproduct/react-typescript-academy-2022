f7d1c3768a4462f739fa05a11d0225e3c07092f0
Trayan Iliev <office@iproduct.org>
2022-08-16 19:45:10 +0300

    TodoInput complete

--- 07-ts-todos-lab/src/TodoInput.tsx
+++ 07-ts-todos-lab/src/TodoInput.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { TodoListener } from './shared-types';
+import { Todo } from './todo-model';
 import './TodoInput.css';
 
 interface TodoInputProps {
@@ -19,12 +20,25 @@ class TodoInput extends Component<TodoInputProps, TodoInputState> {
         this.setState({text: event.target.value});
     }
 
+    handleTodoSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        this.props.onCreateTodo(new Todo(this.state.text));
+        this.setState({text: ''});
+    }
+
+    handleTodoReset = (event: React.FormEvent) => {
+        event.preventDefault();
+        this.setState({text: ''});
+    }
+
     render() {
         return (
-            <form className='TodoInput-form'>
+            <form className='TodoInput' onSubmit={this.handleTodoSubmit}>
                 <label htmlFor='TodoInput-text'>What to do next?</label>
                 <input type='text' id='TodoInput-text' name='text' value={this.state.text} 
                     onChange={this.handleTextChanged} />
+                <button className='button button5' type='submit'>Add TODO</button>
+                <button className='button button3' type='reset' onClick={this.handleTodoReset}>Reset</button>
             </form>
         );
     }
