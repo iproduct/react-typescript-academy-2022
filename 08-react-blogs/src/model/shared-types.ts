5ff98fbfa8a734e0074020b88f32ed09d62d5f91
Trayan Iliev <office@iproduct.org>
2022-08-30 19:33:52 +0300

    json server backend and db.json sample posts added

--- 08-react-blogs/src/model/shared-types.ts
+++ 08-react-blogs/src/model/shared-types.ts
@@ -1,1 +1,29 @@
-export type IdType = number
+import { Post } from "./posts";
+
+export interface PostListener {
+    (todo: Post): void;
+}
+
+export interface Identifiable<K> {
+    id: K;
+}
+
+export type IdType =  number;
+
+export type Optional<V> = V | undefined
+
+export type Partial<V> = {
+    [P in keyof V]?: V[P];
+};
+
+export type Concrete<V> = {
+    [P in keyof V]-?: V[P];
+};
+
+export type Mutable<V> = {
+    -readonly [P in keyof V]: V[P];
+};
+
+export type Immutable<V> = {
+    readonly [P in keyof V]: V[P];
+};
