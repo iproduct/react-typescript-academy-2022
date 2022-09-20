a0dd13335ab2a2e392873ce4651acfbb7be54786
Trayan Iliev <office@iproduct.org>
2022-09-20 23:06:11 +0300

    routing config demo added

---
+++ 11-router-basics-ts/src/pages/RootPage.tsx
@@ -0,0 +1,48 @@
+import { Link, Outlet } from "react-router-dom";
+import './RootPage.css';
+
+export default function RootPage() {
+  return (
+    <div className="RootPage-container">
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+          <form id="search-form" role="search">
+            <input
+              id="q"
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+            />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={true}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </form>
+          <form method="post">
+            <button type="submit">New</button>
+          </form>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/contacts/1`}>Your Name</Link>
+            </li>
+            <li>
+              <Link to={`/contacts/2`}>Your Friend</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="detail">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
