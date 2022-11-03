1602622e0cb968d8b073598845c89f50c049c9ad
Trayan Iliev <office@iproduct.org>
2022-11-03 20:27:33 +0200

    all users promise composition

--- 03-fetch-api-lab/js/github-promises.js
+++ 03-fetch-api-lab/js/github-promises.js
@@ -6,15 +6,23 @@ function init() {
         .then(usersResp => usersResp.json())
         .then(users => {
             console.log(users);
-            // resultsElem.innerHTML = JSON.stringify(users);
-            return fetch(`https://api.github.com/users/${users[0].username}`)
-        }).then(gitUserResp => gitUserResp.json())
-        .then(gitUser => {
-            console.log(gitUser);
-            const img = new Image();
-            img.src = gitUser.avatar_url;
-            resultsElem.appendChild(img);
-        });
+            return Promise.allSettled(
+                users.map(user => fetch(`https://api.github.com/users/${user.username}`)
+                    .then(resp => resp.json())
+                )
+            );
+        }).then(gitUsers =>
+            gitUsers.filter(gitUser => gitUser.status === 'fulfilled')
+                .map(gitUser => {
+                    console.log(gitUser.value);
+                    const img = new Image();
+                    img.src = gitUser.value.avatar_url;
+                    resultsElem.appendChild(img);
+                    return img;
+                })
+        ).then(images => new Promise((resolve, reject) => setTimeout(resolve, 10000, images)))
+        .then(images => images.forEach(img => img.remove()))
+        .finally(()=> console.log("Demo finished."));
 }
 
 init();
