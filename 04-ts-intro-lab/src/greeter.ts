ccde2643c27ab9f026d25b6c80c13cf89c706dce
Trayan Iliev <office@iproduct.org>
2022-11-11 21:03:13 +0200

    classes and interfaces demo with inheritance

--- 04-ts-intro-lab/src/greeter.ts
+++ 04-ts-intro-lab/src/greeter.ts
@@ -1,10 +1,15 @@
-function greeter(person: string) {
-    return `Hi ${person} from TypeScript!`
-}
+import { Role, User, UserBase } from './users.js';
 
-const user: string = 'Trayan';
+// function greeter(person: string) {
+//     return `Hi ${person} from TypeScript!`
+// }
+
+const user: User = new UserBase('Default', 'Admin', 'admin@mycompany.com', 'admin123', [Role.READER, Role.AUTHOR, Role.ADMIN]);
+
+// document.getElementById('results')!.innerHTML = greeter(user);
 
 const resultsElem = document.getElementById('results');
 if(resultsElem) {
-    resultsElem.innerHTML = greeter(user);
+    resultsElem.innerHTML = user.salutation;
 }
+
---
+++ 04-ts-intro-lab/src/users.ts
@@ -0,0 +1,80 @@
+import { IdType } from "./common-types.js";
+import { Contact, Person } from "./person.js";
+
+export interface User extends Person {
+    password: string;
+    roles: Role[]; // Array<Role>
+    readonly salutation: string;
+    toString(): string;
+    // toString: () => string;
+}
+
+export enum Role {
+    AUTHOR = 1, READER, ADMIN
+}
+
+export class UserBase implements User {
+    id: IdType = undefined;
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public password: string,
+        public roles: Role[],
+        public contact?: Contact | undefined
+    ) {}
+    get salutation() {
+        return `Hello ${this.firstName} ${this.lastName} in roles: ${this.roles.map(r => Role[r]).join(', ')}`;
+    }
+    toString(): string {
+        return `ID: ${this.id}, ${this.salutation}`;
+    }
+}
+
+export class Reader extends UserBase {
+    constructor(
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        roles: Role[] = [Role.READER],
+        contact?: Contact | undefined
+    ) {
+        super(firstName, lastName, email, password, roles, contact);
+    }
+    toString(): string {
+        return `READER: ${super.toString()}`;
+    }
+}
+
+export class Author extends UserBase {
+    constructor(
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        roles: Role[] = [Role.AUTHOR],
+        contact?: Contact | undefined
+    ) {
+        super(firstName, lastName, email, password, roles, contact);
+    }
+    toString(): string {
+        return `AUTHOR: ${super.toString()}`;
+    }
+}
+
+export class Admin extends UserBase {
+    constructor(
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        roles: Role[] = [Role.ADMIN],
+        contact?: Contact | undefined
+    ) {
+        super(firstName, lastName, email, password, roles, contact);
+    }
+    toString(): string {
+        return `ADMIN: ${super.toString()}`;
+    }
+}
