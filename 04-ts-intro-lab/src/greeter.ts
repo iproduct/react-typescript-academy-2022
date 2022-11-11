94d2bdc860bca428777e77a1bee8123ed3fe4d6c
Trayan Iliev <office@iproduct.org>
2022-11-11 21:14:05 +0200

    users demo added

--- 04-ts-intro-lab/src/greeter.ts
+++ 04-ts-intro-lab/src/greeter.ts
@@ -1,15 +1,24 @@
-import { Role, User, UserBase } from './users.js';
+import { Admin, Author, Reader, Role, User } from './users.js';
 
 // function greeter(person: string) {
 //     return `Hi ${person} from TypeScript!`
 // }
 
-const user: User = new UserBase('Default', 'Admin', 'admin@mycompany.com', 'admin123', [Role.READER, Role.AUTHOR, Role.ADMIN]);
-
+const users: User[] = [
+    new Admin('Default', 'Admin', 'admin@mycompany.com', 'admin123', [Role.READER, Role.AUTHOR, Role.ADMIN]),
+    new Author('Default', 'Author', 'author@mycompany.com', 'author123'),
+    new Reader('Default', 'Reader', 'reader@mycompany.com', 'reader123'),
+    new Author('John', 'Doe', 'john@gmail.com', 'john123'),
+    new Admin('Jane', 'Doe', 'jane@gmail.com', 'jane123'),
+];
 // document.getElementById('results')!.innerHTML = greeter(user);
 
 const resultsElem = document.getElementById('results');
-if(resultsElem) {
-    resultsElem.innerHTML = user.salutation;
+if (resultsElem) {
+    users.forEach(user => {
+        const elem = document.createElement('div');
+        elem.innerHTML = user.toString();
+        resultsElem.insertAdjacentElement('beforeend', elem);
+    });
 }
 
