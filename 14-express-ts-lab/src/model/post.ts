bd5dc0bec8dc749b8e497c5a27096a4d6027fdde
Trayan Iliev <office@iproduct.org>
2023-01-06 22:50:21 +0200

    mysql connection pooling demo added

--- 14-express-ts-lab/src/model/post.ts
+++ 14-express-ts-lab/src/model/post.ts
@@ -6,6 +6,7 @@ export class Post {
         public authorId: string,
         public imageUrl: string,
         public tags: string[],
+        public categories: string[],
         public created: Date,
         public modified: Date,
     ) { }
